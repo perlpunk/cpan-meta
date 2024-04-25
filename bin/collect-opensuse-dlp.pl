db2a2ff5fde27949e9627fc43c8cb997a0a1dfaf
Tina Müller <tina.mueller@suse.com>
2024-04-25 12:54:28 +0200

    Generate deps.yaml and patches.yaml

--- bin/collect-opensuse-dlp.pl
+++ bin/collect-opensuse-dlp.pl
@@ -6,24 +6,78 @@
 
 use v5.38;
 use feature 'signatures';
+use autodie;
 use YAML::PP;
+use YAML::PP::Common qw/ YAML_LITERAL_SCALAR_STYLE :PRESERVE /;
 use File::Find;
 
+my $ypp_pres = YAML::PP->new( preserve => PRESERVE_SCALAR_STYLE );
+
+
 my %pkg;
+my %deps;
+my %patches;
 
 sub main ($dir, @args) {
     find \&wanted, $dir;
     my $test = $pkg{"perl-psh"};
-    for my $p (keys %pkg) {
+    for my $p (sort keys %pkg) {
         unless ($pkg{ $p }->{conf}) {
             delete $pkg{ $p };
             next;
         }
-        $pkg{ $p }->{spec} =~ s/\Q$dir//;
+        my $pkgconf = $pkg{ $p };
+        $pkg{ $p }->{specfile} =~ s/\Q$dir//;
+        my $conf = $pkg{ $p }->{conf};
+        delete $conf->{patches};
+        my $version = $pkg{ $p }->{version};
+        if (my $preamble = $conf->{preamble}) {
+            my @preamble = split m/\n/, $preamble;
+            my $skip = 0;
+            for my $line (@preamble) {
+                if ($line =~ m/^%endif /) {
+                    $skip = 0;
+                    next;
+                }
+                next if $skip;
+                next if $line =~ m/^ *$/;
+                next if $line =~ m/^Obsoletes:/;
+                next if $line =~ m/^Provides:/;
+                next if $line =~ m/^Source/;
+                next if $line =~ m/^ExcludeArch/;
+                next if $line =~ m/^%define/;
+                next if $line =~ m/^%requires_eq/;
+                last if $line =~ m/^%post/;
+                next if $line =~ m/^%package/;
+                next if $line =~ m/^Summary:/;
+                next if $line =~ m/^Group:/;
+                next if $line =~ m/^Requires\(post\):/;
+                last if $line =~ m/^%description/;
+                next if $line =~ m/^ *#/;
+                if ($line =~ m/^%if /) {
+                    $skip = 1;
+                    next;
+                }
+                if ($line =~ m/^(Build)?Requires: +(.*)/) {
+                    my $type = $1 ? 'build' : 'runtime';
+                    push @{ $deps{ $p }->{ $version }->{ $type } }, $2;
+                    next;
+                }
+                if ($line =~ m/^(Recommends|Suggests): +(.*)/) {
+                    push @{ $deps{ $p }->{ $version }->{lc $1} }, $2;
+                    next;
+                }
+                die "!!!!! $p: '$line'";
+            }
+        }
+        if (my $patchlist = delete $pkgconf->{patches}) {
+            $patches{ $p }->{ $version } = $patchlist;
+        }
     }
     YAML::PP::DumpFile('cpanspec.yaml', \%pkg);
+    YAML::PP::DumpFile('deps.yaml', \%deps);
+    $ypp_pres->dump_file('patches.yaml', \%patches);
 }
-
 
 sub wanted {
     return if $File::Find::dir =~ m/\.osc/;
@@ -35,9 +89,37 @@ sub wanted {
     if ($_ eq 'cpanspec.yml') {
         my $conf = YAML::PP::LoadFile($_);
         $pkg{ $name }->{conf} = $conf;
+        if (my $patches = $conf->{patches}) {
+            for my $file (sort keys %$patches) {
+                $file =~ s{.*/}{};
+                $file =~ s/%name/$name/;
+                next if $file =~ m/^%/;
+                next unless -f $file; # :(
+                my $opt = $patches->{ $file };
+                open my $fh, '<:encoding(UTF-8)', $file;
+                my $patch = do { local $/; <$fh> };
+                close $fh;
+                push @{ $pkg{ $name }->{patches} }, {
+                    name => $file,
+                    opt => $opt,
+                    content => YAML::PP->preserved_scalar($patch, style => YAML_LITERAL_SCALAR_STYLE),
+                };
+            }
+        }
     }
     elsif (m/\.spec$/) {
-        $pkg{ $name }->{spec} = $File::Find::name;
+        $pkg{ $name }->{specfile} = $File::Find::name;
+        open my $fh, '<', $_;
+        my $spec = do { local $/; <$fh> };
+        close $fh;
+        my $version = '-';
+        if ($spec =~ m/^Version:\s+(\S+)/m) {
+            $version = $1;
+        }
+        else {
+            warn "$name: no Version";
+        }
+        $pkg{ $name }->{version} = $version;
     }
 
 }
